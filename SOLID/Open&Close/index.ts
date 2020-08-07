e6938515c4636d15ab0c5b8f3b48d4d892675534
SirawichDev <sirawit0676@gmail.com>
2020-08-07 17:50:26 +0700

    .

---
+++ SOLID/Open&Close/index.ts
@@ -0,0 +1,48 @@
+class ErrorHandler {
+  private messageBox: any;
+  constructor(messageBox: any) {
+    this.messageBox = messageBox;
+  }
+
+  wrapError(err, publicRes, magnitude) {
+    let error = {
+      originalError: err,
+      publicRes,
+      magnitude,
+    };
+    if (magnitude < 5) {
+      this.handleWarning("Warning", publicRes);
+    } else {
+      this.handleError("Critical Bug", publicRes);
+    }
+  }
+
+  private handleWarning(strHeader, strRes) {
+    this.messageBox.show(strHeader, strRes);
+  }
+  private handleError(strHeader, strRes) {
+    this.messageBox.show(strHeader, strRes);
+  }
+}
+
+class ErrorLogger {
+  private _apiEndpoint: string = "api/v1/log";
+  constructor(private _httpClient) {}
+  toLogError(err): Promise<any> {
+    return this._httpClient.post(this._apiEndpoint, err);
+  }
+}
+
+export class ErrorHandlerLogging extends ErrorHandler {
+  private _logger: ErrorLogger;
+  constructor(messageBox, logger: ErrorLogger) {
+    super(messageBox);
+    this._logger = logger;
+  }
+
+  wrapError(err, publicRes, magnitude) {
+    this._logger.toLogError(err).then(() => {
+      super.wrapError(err, publicRes, magnitude);
+    });
+  }
+}
